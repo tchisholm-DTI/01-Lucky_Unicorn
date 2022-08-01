e290841a6e998f3f7ce4d89e53d9d784ea52b778
tchisholm-DTI <68577478+tchisholm-DTI@users.noreply.github.com>
2022-08-01 14:42:05 +1200

    Added num_check function

--- 00_LU_Base_v1.py
+++ 00_LU_Base_v1.py
@@ -23,10 +23,36 @@ def instructions():
     return ""
 
 
+def num_check(question, low, high):
+    error = "Please enter a whole number between 1 and 10\n"
+
+    valid = False
+    while not valid:
+        try:
+            # Ask the question
+            response = int(input(question))
+
+            # If the amount is too low/too high give
+            if low < response <= high:
+                return response
+
+            # Output an error
+            else:
+                print(error)
+
+        except ValueError:
+            print(error)
+
+
 # Main Routine goes here ...
 played_before = yes_no("Have you played the game before? ")
 
 if played_before == "no":
     instructions()
 
-print("Program continues")
+print()
+
+# Ask user how much they want to play with ...
+how_much = num_check("How much would you like to play with? ", 0, 10)
+
+print("You will be spending ${}".format(how_much))
