6e2da3ffd2bfaae45457783b335c423c94b7da89
tchisholm-DTI <68577478+tchisholm-DTI@users.noreply.github.com>
2022-08-01 19:50:34 +1200

    Add files via upload

---
+++ 05_token_generator_v4.py
@@ -0,0 +1,31 @@
+import random
+
+# Main routine goes here
+
+STARTING_BALANCE = 100
+
+balance = STARTING_BALANCE
+
+# Testing loop to generate 20 tokens
+for item in range(0, 10):
+    chosen_num = random.randint(1, 100)
+
+    # Adjust balance
+    if 1 <= chosen_num <= 5:
+        chosen = "unicorn"
+        balance += 4
+    elif 6 <= chosen_num <= 36:
+        chosen = "donkey"
+        balance -= 1
+    else:
+        if chosen_num % 2 == 0:
+            chosen = "horse"
+        else:
+            chosen = "zebra"
+        balance -= 0.5
+    # Output
+    print("You got a {}. Your balance is: ${:.2f}".format(chosen, balance))
+
+print()
+print("Starting Balance: ${:.2f}".format(STARTING_BALANCE))
+print("Final Balance: ${:.2f}".format(balance))
