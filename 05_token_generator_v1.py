18abae9947be9778c29adca2000f8232242f2d48
tchisholm-DTI <68577478+tchisholm-DTI@users.noreply.github.com>
2022-08-03 11:52:34 +1200

    Created file and added testing loop

--- 05_token_generator_v1.py
+++ 05_token_generator_v1.py
@@ -8,14 +8,6 @@ balance = 100
 # Testing loop to generate 20 tokens
 for item in range(0, 20):
     chosen = random.choice(tokens)
+    print(chosen, end='\t')
 
-    # Adjust balance
-    if chosen == "unicorn":
-        balance += 4
-    elif chosen == "donkey":
-        balance -= 1
-    else:
-        balance -= 0.5
 
-    # Output
-    print("Token: {} , Balance: ${}".format(chosen, balance))
