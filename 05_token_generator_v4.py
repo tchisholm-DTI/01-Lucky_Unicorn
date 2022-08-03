2fce9b0efad8cb20b2b7f8e25f9c72c90f6b5164
tchisholm-DTI <68577478+tchisholm-DTI@users.noreply.github.com>
2022-08-03 12:17:35 +1200

    Added comments

--- 05_token_generator_v4.py
+++ 05_token_generator_v4.py
@@ -11,15 +11,27 @@ for item in range(0, 10):
     chosen_num = random.randint(1, 100)
 
     # Adjust balance
+    # If the random # is between, 1 and 5,
+    # user gets a unicorn (add $4 to balance)
     if 1 <= chosen_num <= 5:
         chosen = "unicorn"
         balance += 4
+
+    # If the random # is between 6 and 36
+    # user gets a donkey (subtract $1 from balance)
     elif 6 <= chosen_num <= 36:
         chosen = "donkey"
         balance -= 1
+
+    # The token is either a horse or zebra ...
+    # In both cases, subtract $0.50 from the balance
     else:
+        # If the number is even, set the chosen
+        # item to a horse
         if chosen_num % 2 == 0:
             chosen = "horse"
+
+        # otherwise set it to a zebra
         else:
             chosen = "zebra"
         balance -= 0.5
