394d03bcfc9251cf0e6bcf44d83b226f8e26a655
tchisholm-DTI <68577478+tchisholm-DTI@users.noreply.github.com>
2022-08-15 20:58:18 +1200

    Added rounds code

--- 00_LU_Base_v1.py
+++ 00_LU_Base_v1.py
@@ -1,3 +1,4 @@
+import random
 # Functions go here ...
 def yes_no(question):
     valid = False
@@ -55,4 +56,54 @@ print()
 # Ask user how much they want to play with ...
 how_much = num_check("How much would you like to play with? ", 0, 10)
 
-print("You will be spending ${}".format(how_much))
+balance = how_much
+
+rounds_played = 0
+
+play_again = input("Press <Enter> to play ...").lower()
+while play_again == "":
+
+    # increase # of rounds played
+    rounds_played += 1
+
+    # print round number
+    print()
+    print("*** Round Number: {} *** ".format(rounds_played))
+
+    chosen_num = random.randint(1, 100)
+
+    # Adjust balance
+    # If the random # is between, 1 and 5,
+    # user gets a unicorn (add $4 to balance)
+    if 1 <= chosen_num <= 5:
+        chosen = "unicorn"
+        balance += 4
+
+    # If the random # is between 6 and 36
+    # user gets a donkey (subtract $1 from balance)
+    elif 6 <= chosen_num <= 36:
+        chosen = "donkey"
+        balance -= 1
+
+    # The token is either a horse or zebra ...
+    # In both cases, subtract $0.50 from the balance
+    else:
+        # If the number is even, set the chosen
+        # item to a horse
+        if chosen_num % 2 == 0:
+            chosen = "horse"
+
+        # otherwise set it to a zebra
+        else:
+            chosen = "zebra"
+        balance -= 0.5
+    # Output
+    print("You got a {}. Your balance is: ${:.2f}".format(chosen, balance))
+    if balance < 1:
+        play_again = "xxx"
+        print("Sorry you have run out of money")
+        print()
+    else:
+        play_again = input("Press enter to play again or 'xxx to quit")
+
+print("Final balance", balance)
